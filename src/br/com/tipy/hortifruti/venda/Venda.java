524aeb1cbd5c2c84d2d7b47b09c22a0fb7df3545
Tiago Oliveira <tiago.santos103@fatec.sp.gov.br>
2020-08-17 14:34:59 -0300

    Teste

--- src/br/com/tipy/hortifruti/venda/Venda.java
+++ src/br/com/tipy/hortifruti/venda/Venda.java
@@ -1,6 +1,13 @@
 package br.com.tipy.hortifruti.venda;
+import java.util.List;
+
+import br.com.tipy.hortifruti.model.sale.item.Item;;
 
 public class Venda {
 	private long cod;
-	private Da
+	private String data;
+	private String cpf;
+	private List<Item> itens;
+	
+	
 }
